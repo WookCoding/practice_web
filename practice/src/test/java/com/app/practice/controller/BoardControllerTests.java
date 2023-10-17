41d84da01513a1dcfadb3804da42c4e713189cd0
WookCoding <dnrwhddla@gmail.com>
2023-10-17 22:30:46 +0900

    작성하기 썸네일

--- practice/src/test/java/com/app/practice/controller/BoardControllerTests.java
+++ practice/src/test/java/com/app/practice/controller/BoardControllerTests.java
@@ -7,8 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
