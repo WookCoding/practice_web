84d4139a4808a4a9d32fa74f6449d84bd8abfb08
WookCoding <dnrwhddla@gmail.com>
2023-10-23 23:47:19 +0900

    로그인 오류 수정완료

--- practice/src/main/java/com/app/practice/domain/dao/MemberDAO.java
+++ practice/src/main/java/com/app/practice/domain/dao/MemberDAO.java
@@ -3,6 +3,7 @@ package com.app.practice.domain.dao;
 import com.app.practice.domain.vo.member.MemberVO;
 import com.app.practice.mapper.MemberMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -10,7 +11,7 @@ import org.springframework.stereotype.Repository;
 @RequiredArgsConstructor
 public class MemberDAO {
 
-    private MemberMapper memberMapper;
+    private final MemberMapper memberMapper;
 
     // 회원 가입
     public void insertMember(MemberVO memberVO){
